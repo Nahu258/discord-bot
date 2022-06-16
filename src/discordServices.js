0852d3f0b52ff3f4f989807d9207747ce5fa2cb0
Nahuel <Nahugc258@gmail.com>
2022-06-16 18:39:45 -0300

    idk

--- src/discordServices.js
+++ src/discordServices.js
@@ -37,9 +37,9 @@ const getStats = async () => {
 
 const setCounter = () => {
   const today = new Date();
-  const arrive = new Date("Fri Jul 1 2022 07:10:00 GMT-0300").toUTCString()
+  const arrive = new Date("Fri Jul 1 2022 07:10:00").toUTCString()
   client.user.setPresence({
-    activities: [{name: `Hours: ${arrive} ◎`,type: 'WATCHING'}],
+    activities: [{name: `:${arrive} ◎`,type: 'WATCHING'}],
     status: 'dnd'
   })
 }
